ee08d6e4904aa9ee1e417d168ce7c5fda6dc33cf
f-PLT <fplt.softwaredeveloper@gmail.com>
2026-01-25 20:30:47 -0500

    Add docstrings and refactor arg names

--- packages/connectors/src/river_connectors/cehq.py
+++ packages/connectors/src/river_connectors/cehq.py
@@ -14,8 +14,19 @@ CEHQ_TIME = "Heure"
 CEHQ_LEVEL = "Débit"
 CEHQ_COLUMNS = frozenset([CEHQ_DATE, CEHQ_TIME, CEHQ_LEVEL])
 
+gauge_info = {"040204": "Rivière Rouge"}
+
 
 def format_date_time(date: str, time: str) -> datetime:
+    """Format a date and time string into a datetime object.
+
+    Args:
+        date: The date string.
+        time: The time string.
+
+    Returns:
+        The formatted datetime object.
+    """
     clean_date = date.strip()
     clean_time = time.strip()
     formatted_date = datetime.strptime(f"{clean_date} {clean_time}", "%Y-%m-%d %H:%M")
@@ -24,6 +35,14 @@ def format_date_time(date: str, time: str) -> datetime:
 
 
 def fetch_cehq_station_data(station_id: str) -> pandas.DataFrame:
+    """Fetch data from the CEHQ API for a specific station.
+
+    Args:
+        station_id: The ID of the station to fetch data for.
+
+    Returns:
+        pandas.DataFrame: The data for the station.
+    """
     station_url = f"{CEHQ_BASE_URL}{station_id}"
     data = requests.get(station_url).text
     df = pandas.read_csv(StringIO(data), sep="\t", lineterminator="\n")
@@ -32,16 +51,28 @@ def fetch_cehq_station_data(station_id: str) -> pandas.DataFrame:
     return df
 
 
-def create_cehq_gauge_readings(section_name: str, df: pandas.DataFrame) -> list[FlowGaugeReading]:
+def create_cehq_gauge_readings(station_id: str, df: pandas.DataFrame) -> list[FlowGaugeReading]:
+    """Create gauge readings from a DataFrame of CEHQ data.
+
+    Args:
+        station_id: The id of the river gauge.
+        df: The DataFrame of CEHQ data.
+
+    Returns:
+        list[FlowGaugeReading]: The list of gauge readings.
+    """
     required_columns = list(CEHQ_COLUMNS)
     if not all(str(col).strip() in df.columns for col in required_columns):
         raise RiverConnectorError(
-            f"Error while creating CEHQ gauge readings for {section_name}: Missing required columns"
+            f"Error while creating CEHQ gauge readings for {station_id}: Missing required columns"
         )
+
+    gauge_name = gauge_info.get(station_id, "Unknown")
 
     readings_list = [
         FlowGaugeReading(
-            river_section=section_name,
+            gauge_id=station_id,
+            gauge_name=gauge_name,
             datetime=format_date_time(date=date, time=time),
             level=flow,
         )
--- packages/core/src/river_core/domain/readings.py
+++ packages/core/src/river_core/domain/readings.py
@@ -5,17 +5,33 @@ from pydantic import BaseModel
 
 
 class LevelType(str, Enum):
+    """Level type."""
+
     RIVER_LEVEL = "river_level"
     FLOW = "flow"
 
 
 class LevelUnit(str, Enum):
+    """Level unit."""
+
     M = "m"
     M3_S = "m3/s"
 
 
 class BaseGaugeReading(BaseModel):
-    river_section: str
+    """Base gauge reading.
+
+    Attributes:
+        gauge_id (str): The id of the gauge.
+        gauge_name (str): The name of the gauge.
+        datetime (datetime): The datetime of the reading.
+        level (str): The level of the reading.
+        level_type (LevelType): The type of the level.
+        level_unit (LevelUnit): The unit of the level.
+    """
+
+    gauge_id: str
+    gauge_name: str
     datetime: datetime
     level: str
     level_type: LevelType
@@ -23,10 +39,24 @@ class BaseGaugeReading(BaseModel):
 
 
 class FlowGaugeReading(BaseGaugeReading):
+    """Flow gauge reading.
+
+    Attributes:
+        level_type (LevelType): The type of the level.
+        level_unit (LevelUnit): The unit of the level.
+    """
+
     level_type: LevelType = LevelType.FLOW
     level_unit: LevelUnit = LevelUnit.M3_S
 
 
 class LevelGaugeReading(BaseGaugeReading):
+    """Level gauge reading.
+
+    Attributes:
+        level_type (LevelType): The type of the level.
+        level_unit (LevelUnit): The unit of the level.
+    """
+
     level_type: LevelType = LevelType.RIVER_LEVEL
     level_unit: LevelUnit = LevelUnit.M
